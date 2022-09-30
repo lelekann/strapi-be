558c0c2598e38987110660c46f93c73185dadee6
lelekann <anna.leleko@eliftech.com>
2022-09-30 09:29:39 +0300

    created be for vue project

---
+++ config/database.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'pet'),
+      user: env('DATABASE_USERNAME', 'petadmin'),
+      password: env('DATABASE_PASSWORD', 'Eliftech1'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      },
+    },
+    debug: false,
+  },
+});
